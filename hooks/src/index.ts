5a7e4675666c7bb128be64ca8c61cc70eb121398
OfficialAyush77 <ayushanand2017@gmail.com>
2024-07-28 08:35:54 +0530

    Added Schema

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,10 +1,43 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const app = express();
+app.use(express.json());
 
-app.post("/hooks/catch/:userId/:zapId",(req,res)=>{
+app.post("/hooks/catch/:userId/:zapId",async(req,res)=>{
     const userId = req.params.userId;
     const zapId = req.params.zapId;
+    const body = req.body;
 
- 
+    console.log("Reached Here");
+    
+    // Store a DB in new trigger 
+
+    await client.$transaction(async tx=>{
+        console.log("Reached Here 2");
+        
+       const run =  await client.zapRun.create({
+            data: {
+                zapId:zapId,
+                metadata:body
+            }
+            
+        });
+        console.log("Reached Here 3");
+        await client.zapRunOutbox.create({
+             data:{
+                zapRunId : run.id
+             }
+        })
+    })
+
+    res.json({
+        message:"Webhooks Received"
+    })
+
+    // Push into A queue in  Kafka / Redis 
 })
+
+app.listen(3000);
